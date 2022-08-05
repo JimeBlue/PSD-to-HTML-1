7e70c98a63668a94ff997f4e48582d963c93ad8c
Jimena <jimeblue69@gmail.com>
2022-08-05 19:24:12 +0200

    Finish selectors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // SELECTORS
+  // ---All p tags insise #lead-banner---------
+  //   $('#lead-banner p');
+  // ----Element selectors---------------------
+  //   $('h3').css({ border: '3px solid blue' });
+  // ----Class selectors---------------------
+  //   $('.wrapper').css({ border: '3px solid blue' });
+  // ----ID selectors---------------------
+  //   $('#clients').css({ border: '3px solid yellow' });
+});
