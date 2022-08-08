ad8acb9cc56ac220229afbb79407a29ac0a529d4
Jimena <jimeblue69@gmail.com>
2022-08-08 16:17:01 +0200

    Finish with chaining

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // Chaining allows us to link together various jQuery methods in one statement
+  $('#contact-methods')
+    .css({ border: '3px solid blue' })
+    .next()
+    .css({ border: '5px solid pink' })
+    .closest('section')
+    .css({ border: '3px solid yellow' });
+});
